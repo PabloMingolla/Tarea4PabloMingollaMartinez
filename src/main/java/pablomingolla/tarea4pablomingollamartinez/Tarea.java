7f1727ea0f45a9028423f3aac6efc19576b187d3
PabloMingolla <74044014+PabloMingolla@users.noreply.github.com>
2020-12-07 11:45:53 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/pablomingolla/tarea4pablomingollamartinez/Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pablomingolla.tarea4pablomingollamartinez;
+
+/**
+ *
+ * @author Pablo Mingolla
+ */
+public class Tarea {
+    double peso;
+    boolean listado;
+    int contador;
+}
